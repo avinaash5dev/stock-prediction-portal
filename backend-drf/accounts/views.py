e7fc87029d45a35a493f449c089f019fe6e73a26
avi-1227 <avinaashprasadd@gmail.com>
2025-12-16 09:59:09 +0530

    create UserSerializer and RegisterView

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import generics # pyright: ignore[reportMissingImports]
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_class = [AllowAny]
