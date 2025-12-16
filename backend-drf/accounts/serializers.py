7b199e3381ed742995fc70afebf34345b2ec7887
avi-1227 <avinaashprasadd@gmail.com>
2025-12-16 10:23:50 +0530

    Created url in api apps and mentioned sytle type and min length in password in accounts serializer

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers # pyright: ignore[reportMissingImports]
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8, style={'input_type':'password'})
     class Meta:
         model = User
         fields = ["username", "email", "password"] 
